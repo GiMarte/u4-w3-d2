1433268a80a63fa2eb331956b2fcbed95acd8ae2
Hammers <gianluca.hammers@gmail.com>
2026-01-20 14:31:15 +0100

    Initial commit

---
+++ src/test/java/gianmarte/ApplicationTest.java
@@ -0,0 +1,13 @@
+package gianmarte;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
